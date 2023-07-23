e9156e4703344c72d3c8dade5944362bfa2975da
Schouwey <zaid.schouwey@gmail.com>
2023-07-23 20:13:56 +0200

    remove unused code

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -6,27 +6,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + "/../../../config.json")[env];
 
 interface Database {
   sequelize: Sequelize;
   Sequelize: typeof Sequelize;
 }
-
 const db: Database = {} as Database;
 
-let sequelize: Sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  dialect: 'mariadb',
+})
 
 fs.readdirSync(__dirname)
   .filter((file) => {
