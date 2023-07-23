25c9736de29527392631c62ee524b8a87a13865d
Schouwey <zaid.schouwey@gmail.com>
2023-07-23 20:06:44 +0200

    token on certain routes

--- src/v1/routes/equipmentRoutes.ts
+++ src/v1/routes/equipmentRoutes.ts
@@ -1,13 +1,14 @@
 import { EquipmentController } from "../../controllers/equipmentController";
 import { Router } from 'express'
+import { auth } from "../../middleware/auth";
 const router = Router();
 const equipmentController = new EquipmentController();
 
 
 router.get("/", equipmentController.getAllEquipments.bind(equipmentController));
 router.get("/:equipmentId", equipmentController.getEquipmentById.bind(equipmentController));
-router.post("/",equipmentController.createEquipment.bind(equipmentController));
-router.patch("/:equipmentId", equipmentController.updateEquipment.bind(equipmentController));
-router.delete("/:equipmentId", equipmentController.deleteEquipment.bind(equipmentController));
+router.post("/", auth,equipmentController.createEquipment.bind(equipmentController));
+router.patch("/:equipmentId",auth, equipmentController.updateEquipment.bind(equipmentController));
+router.delete("/:equipmentId",auth, equipmentController.deleteEquipment.bind(equipmentController));
 
 export default router;
